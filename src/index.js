8c5989dda6a71b720983c08726f3fb151dbc7d32
bulzaj <ernestsado94@gmail.com>
2020-08-24 18:25:58 +0200

    added redux

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ import './cssVariables.css';
 import App from './containers/app/App';
 import * as serviceWorker from './serviceWorker';
 
+import {store} from "./state/store/store";
+import {Provider} from 'react-redux'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
