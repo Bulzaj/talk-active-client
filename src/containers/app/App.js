d669839de48717b266ec9dea727f5637a41ca989
bulzaj <ernestsado94@gmail.com>
2020-08-30 20:40:42 +0200

    added routes

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom'
 import Layout from "../layout/Layout";
 
 import styles from './App.module.css'
+import Home from "../../pages/home/Home";
+import Communicator from "../../pages/communicator/Communicator";
 
 function App() {
+
+  let routes = (
+    <Switch>
+      <Route path='/talk' component={Communicator}/>
+      <Route path='/' exact component={Home}/>
+    </Switch>
+  )
+
   return (
     <div className={styles.App}>
-      <Layout/>
+      <Layout>
+        {routes}
+      </Layout>
     </div>
   );
 }
