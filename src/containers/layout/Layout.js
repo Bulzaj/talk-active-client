9aacbb72e5bee81c1e74ec0f6dcff2927fa5408c
bulzaj <ernestsado94@gmail.com>
2020-09-01 12:39:18 +0200

    replacing navbar with material ui lib

--- src/containers/layout/Layout.js
+++ src/containers/layout/Layout.js
@@ -1,36 +1,12 @@
 import React from "react";
 
 import styles from './Layout.module.css'
-import Navbar from "../../components/navbar/Navbar";
-import NavItem from "../../components/navbar/nav-item/NavItem";
-import NavLink from "../../components/navbar/nav-link/NavLink";
-
-import {faCoffee} from "@fortawesome/free-solid-svg-icons";
 
 const Layout = props => {
   return (
     <div className={styles.Layout}>
       <nav>
-        <Navbar>
-          <NavItem>
-            <NavLink
-              icon={faCoffee}
-              linkText='Login'
-            />
-          </NavItem>
-          <NavItem>
-            <NavLink
-              icon={faCoffee}
-              linkText='coffee'
-            />
-          </NavItem>
-          <NavItem>
-            <NavLink
-              icon={faCoffee}
-              linkText='coffee'
-            />
-          </NavItem>
-        </Navbar>
+
       </nav>
       <main>
         {props.children}
