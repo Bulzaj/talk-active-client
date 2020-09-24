87229dd3be0ebca728c9b16cf85e795fe04ca16f
bulzaj <ernestsado94@gmail.com>
2020-09-24 23:16:00 +0200

    edited home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,28 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import {Typography} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import {Link} from "react-router-dom";
 
 const Home = props => {
+
   return (
-    <h1>Home</h1>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: '100%' }}
+    >
+
+      <Grid item xs={3}>
+        <Typography>Welcome</Typography>
+        <Typography>And</Typography>
+        <Button><Link to='/talk'>GetStarted</Link></Button>
+      </Grid>
+
+    </Grid>
   )
 }
 
